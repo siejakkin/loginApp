dda543f4030fc0941e0d731041cb9d9f062024f5
Kinga Siejak <siejakkinga03@gmail.com>
2022-09-08 14:51:46 +0200

    Zadania

---
+++ login-app-back/server.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+//server settings
+var server = {
+  port: 4000
+}
+
+//database settings
+db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'DB_LoginApp'
+})
+
+const generateToken = () => {
+  let token = ''
+  for(let i = 0; i < 80; i++){
+    token += String.fromCharCode(0 + parseInt(Math.floor(Math.random() * (127 - 33 + 1) + 33)))
+  }
+  return token
+}
+
+app.use(cors())
+app.use(bodyParser.json());
+
+// starting the server
+app.listen( server.port , () => console.log(`Server started, listening port: ${server.port}`))
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js');
+});
+
+app.get('/dbTest', (req, res) => { 
+  const sqlQuery = "SHOW TABLES"
+  db.query(sqlQuery, (err, result) => {
+      console.log(result)
+      res.send(result)
+  })
+});
+
+app.get('/usersList', (req, res) => { 
+  const sqlQuery = "Select * from Users";
+  db.query(sqlQuery, (err, result) => {
+      console.log(result)
+      res.send(result)
+  })
+});
+
+app.post('/login', (req, res) => { 
+  const login = req.body.login
+  const pwd = req.body.pwd
+
+  const sqlQuery = "Select * from Users where Login like (?) and Password like (?)";
+
+  db.query(sqlQuery, [login, pwd], (err, result) => {
+    console.log(result)
+    if(err) res.send(err)
+    else if(result.length === 0) res.json({
+      auth: false,
+      token: null})
+    else res.json({
+      auth: true,
+      token: generateToken()})
+  })
+});
+export const getToken = () => {
+  if(localStorage.getItem('token') !== null) return true;
+  return false;
+}
---
+++ login-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import HomePage from './components/HomePage';
+import ErrorPage from './components/ErrorPage';
+import LoginPage from './components/LoginPage';
+import { BrowserRouter, Route, Routes, Outlet, Navigate } from 'react-router-dom';
+import { getToken } from './utils/session'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+        <Route path="" exac element={<LoginPage />} />
+        <Route exact path="HomePage" element={getToken() ? <HomePage /> : <Navigate to='/' replace />}/>
+        <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
